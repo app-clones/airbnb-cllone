1680faea7314490c3d7293236f93096ab0cd4f41
Beatzoid <43425381+Beatzoid@users.noreply.github.com>
2021-04-25 17:13:46 -0700

    ✨ Add Basic Jest Testing

--- packages/server/src/index.ts
+++ packages/server/src/index.ts
@@ -5,11 +5,13 @@ import { importSchema } from "graphql-import";
 import { resolvers } from "./resolvers";
 import { createConnection } from "typeorm";
 
-const typeDefs = importSchema("src/schema.graphql");
+export const startServer = async () => {
+    const typeDefs = importSchema("src/schema.graphql");
 
-const server = new GraphQLServer({ typeDefs, resolvers });
-createConnection().then(() => {
-    server.start(() =>
-        console.log("Server is running on http://localhost:4000")
-    );
-});
+    const server = new GraphQLServer({ typeDefs, resolvers });
+    await createConnection();
+    await server.start();
+    console.log("Server is running on http://localhost:4000");
+};
+
+startServer();
