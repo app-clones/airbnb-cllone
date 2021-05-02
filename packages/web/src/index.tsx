82ee4439223eb9f1ecc1e0ecaccd2be9d654a0ef
Beatzoid <43425381+Beatzoid@users.noreply.github.com>
2021-05-02 16:51:32 -0700

    ✨ Setup Apollo Client and React Router

--- packages/web/src/index.tsx
+++ packages/web/src/index.tsx
@@ -1,11 +1,12 @@
-import React from "react";
 import ReactDOM from "react-dom";
-import "./index.css";
-import App from "./App";
+
+import { ApolloProvider } from "@apollo/client";
+import { client } from "./apollo";
+import { Routes } from "./routes";
 
 ReactDOM.render(
-    <React.StrictMode>
-        <App />
-    </React.StrictMode>,
+    <ApolloProvider client={client}>
+        <Routes />
+    </ApolloProvider>,
     document.getElementById("root")
 );
---
+++ packages/web/src/modules/register/RegisterConnector.tsx
@@ -0,0 +1,1 @@
+export const RegisterConnector = () => <div>Register</div>;
