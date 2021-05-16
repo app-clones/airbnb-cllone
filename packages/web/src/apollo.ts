96c616211ccb3c0d7d1b649f5b64157b781c4d1b
Beatzoid <43425381+Beatzoid@users.noreply.github.com>
2021-05-16 12:21:01 -0700

    ✨ Netlify

--- packages/web/src/apollo.ts
+++ packages/web/src/apollo.ts
@@ -1,7 +1,7 @@
 import { ApolloClient, InMemoryCache } from "@apollo/client";
 
 export const client = new ApolloClient({
-    uri: "http://localhost:4000",
+    uri: process.env.REACT_APP_SERVER_URL,
     credentials: "include",
     cache: new InMemoryCache()
 });
